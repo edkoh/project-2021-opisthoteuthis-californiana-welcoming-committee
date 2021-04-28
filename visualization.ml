adaaef217c9f5fb6d4e8e0e004eaa2987383b792
Edward Koh <ekoh1@college.harvard.edu>
2021-04-27 18:09:50 -0700

    experiments

--- visualization.ml
+++ visualization.ml
@@ -8,7 +8,12 @@
 open Config ;;
 module G = Graphics ;;
 
-while true do
-  let c = G.read_key () in
-  Printf.printf c
-done
+let rec loop () =
+  let event = G.wait_next_event [Key_pressed] in
+  (if event.keypressed then (Printf.printf "%c\n" event.key) else Printf.printf "_");
+  loop ()
+
+let _ =
+  G.open_graph "";
+  loop ();
+  G.close_graph ();
