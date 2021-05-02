d2d1bc0a6a531c1ef4c3f9ab22e7b15e2a3dd244
Flixnore <felixperezdiener@gmail.com>
2021-05-02 10:03:54 -0400

    remove unnecessary clear function

--- model.ml
+++ model.ml
@@ -15,14 +15,6 @@ type model = bool array array ;;
 let sq_full ((posx, posy) : int * int) (m : model) : bool =
   posx < 0 || posx > cBOARD_X - 1 || posy < 0 || (posy < cBOARD_Y && m.(posy).(posx))
 
-let clear (m : model) : unit =
-  for i = 0 to (Array.length m) - 1 do
-    for j = 0 to (Array.length m.(i)) - 1 do
-      m.(i).(j) <- false
-    done
-  done
-;;
-
 let clear_lines (m : model) : int =
   let shift = ref 0 in
   for row = 0 to cBOARD_Y - 1 do
