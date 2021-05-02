55d78c1b4103b257edfa7103092e3dee73178e56
Flixnore <felixperezdiener@gmail.com>
2021-05-01 22:18:59 -0400

    comment and change hardcoded values to config values

--- controller.ml
+++ controller.ml
@@ -3,10 +3,13 @@
                             OCaml Tetris
                         Visualizing the game
  *)
+
 open Config ;;
 module G = Graphics ;;
 
-(* on_capture () *)
+(* on_capture () -- Returns the action from the corresponding keypress.
+                    Returns NoAction if no key is pressed, or if the key
+                    does not correspond to an action. *)
 let on_capture () : action =
   if G.key_pressed () then
     match G.read_key () with
