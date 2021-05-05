c1076e22d41cd67490a67c97469f9c150f21c727
Edward Koh <ekoh1@college.harvard.edu>
2021-05-05 16:48:58 -0700

    smol indent

--- tetris.ml
+++ tetris.ml
@@ -35,37 +35,37 @@ let _ =
   let level = ref 1 in
 
   try
-  let queued = ref (random_piece ()) in
-  while true do
-    let piece = !queued in
-    queued := random_piece ();
-    while piece#move m Down do
-      V.render_model m;
-      V.render_text !score !level;
-      piece#draw;
-      !queued#draw_queue;
-      let tickspeed = level_tick_formula !level in (* slight optimization *)
-      let now = Sys.time () in
-      let dropped = ref false in
-      while (Sys.time () -. now < tickspeed) && not !dropped do
-        let input = on_capture () in
-        if piece#move m input then (
-          V.render_model m;
-          V.render_text !score !level;
-          piece#draw;
-          !queued#draw_queue;
-        );
-        if input = Drop then dropped := true
+    let queued = ref (random_piece ()) in
+    while true do
+      let piece = !queued in
+      queued := random_piece ();
+      while piece#move m Down do
+        V.render_model m;
+        V.render_text !score !level;
+        piece#draw;
+        !queued#draw_queue;
+        let tickspeed = level_tick_formula !level in (* slight optimization *)
+        let now = Sys.time () in
+        let dropped = ref false in
+        while (Sys.time () -. now < tickspeed) && not !dropped do
+          let input = on_capture () in
+          if piece#move m input then (
+            V.render_model m;
+            V.render_text !score !level;
+            piece#draw;
+            !queued#draw_queue;
+          );
+          if input = Drop then dropped := true
+        done;
       done;
+      if List.exists (fun (_, y) -> y >= cBOARD_Y) piece#get_pos then
+        raise (GameOver ("GAME OVER. FINAL SCORE: " ^ string_of_int !score ^ "\n"))
+        (* implement game over screen if there was more time*)
+      else
+        piece#add_to_model m;
+        score := !score + (clear_lines m);
+        level := !score / 5 + 1;
+        (* G.sound 800 200; Graphics sound function not compatible with system *)
     done;
-    if List.exists (fun (_, y) -> y >= cBOARD_Y) piece#get_pos then
-      raise (GameOver ("GAME OVER. FINAL SCORE: " ^ string_of_int !score ^ "\n"))
-      (* implement game over screen if there was more time*)
-    else
-      piece#add_to_model m;
-      score := !score + (clear_lines m);
-      level := !score / 5 + 1;
-      (* G.sound 800 200; Graphics sound function not compatible with system *)
-  done;
   with
     | GameOver s -> Printf.printf "%s" s
